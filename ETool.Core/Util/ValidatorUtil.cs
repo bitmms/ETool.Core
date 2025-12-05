f838256d4e59a8daf7ecb4c9c7e6f3eb97deef75
bitm <1759743261@qq.com>
2025-12-06 01:36:08 +0800

    新增：手机号码格式校验（ValidatorUtil.IsValidPhoneNumber）

--- ETool.Core/Util/ValidatorUtil.cs
+++ ETool.Core/Util/ValidatorUtil.cs
@@ -5,5 +5,45 @@
     /// </summary>
     public static class ValidatorUtil
     {
+        /// <summary>
+        /// 校验入参 <c>str</c> 是否符合中国大陆 11 位手机号码格式规范
+        /// </summary>
+        /// <param name="str">待校验的字符串</param>
+        /// <returns>符合格式返回 <c>true</c>，否则返回 <c>false</c></returns>
+        public static bool IsValidPhoneNumber(string str)
+        {
+            if (StrUtil.IsNull(str) || StrUtil.IsEmpty(str) || StrUtil.IsAllWhiteSpace(str))
+            {
+                return false;
+            }
+
+            if (str.Length != 11)
+            {
+                return false;
+            }
+
+            // 手机号码第 1 位：必须是数字 1
+            if (str[0] != '1')
+            {
+                return false;
+            }
+
+            // 手机号码第 2 位：必须是 3-9 的数字
+            if (str[1] < '3' || str[1] > '9')
+            {
+                return false;
+            }
+
+            // 手机号码后 9 位：必须是 0-9 的数字
+            for (int i = 2; i < 11; i++)
+            {
+                if (!CharUtil.IsDigit(str[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
