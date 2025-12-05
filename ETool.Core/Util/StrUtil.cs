e1f2399f7307cb78fa0c1e643f2369d8ef433bbc
bitm <1759743261@qq.com>
2025-12-06 00:23:32 +0800

    新增：字符串是否为空判断（StrUtil.IsEmpty）

--- ETool.Core/Util/StrUtil.cs
+++ ETool.Core/Util/StrUtil.cs
@@ -14,5 +14,15 @@
         {
             return s == null;
         }
+
+        /// <summary>
+        /// 判断入参 <c>s</c> 是否为 <c>""</c>
+        /// </summary>
+        /// <param name="s">待判断的字符串</param>
+        /// <returns>若 <c>s</c> 为 <c>""</c> 返回 <c>true</c>，否则返回 <c>false</c></returns>
+        public static bool IsEmpty(string s)
+        {
+            return s != null && s.Length == 0;
+        }
     }
 }
