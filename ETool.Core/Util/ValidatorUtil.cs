a562b797fb915f8beffb0beb2bd5e219d1a28823
bitm <1759743261@qq.com>
2025-12-06 01:37:30 +0800

    新增：正整数格式校验（ValidatorUtil.IsValidPositiveNumber）

--- ETool.Core/Util/ValidatorUtil.cs
+++ ETool.Core/Util/ValidatorUtil.cs
@@ -45,5 +45,36 @@
 
             return true;
         }
+
+        /// <summary>
+        /// 校验入参 <c>str</c> 是否为正整数
+        /// </summary>
+        /// <param name="str">待校验的字符串</param>
+        /// <returns>符合格式返回 <c>true</c>，否则返回 <c>false</c></returns>
+        public static bool IsValidPositiveNumber(string str)
+        {
+            if (StrUtil.IsNull(str) || StrUtil.IsEmpty(str) || StrUtil.IsAllWhiteSpace(str))
+            {
+                return false;
+            }
+
+            // 不允许前导零的存在
+            if (str[0] == '0')
+            {
+                return false;
+            }
+
+            // 逐字符检查是否为数字
+            int len = str.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (!CharUtil.IsDigit(str[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
