31be36a94dec52c344a79c668ce13177db777218
bitm <1759743261@qq.com>
2025-12-06 00:19:38 +0800

    新增：字符串是否为 null 判断（StrUtil.IsNull）

--- ETool.Core/Util/StrUtil.cs
+++ ETool.Core/Util/StrUtil.cs
@@ -5,5 +5,14 @@
     /// </summary>
     public static class StrUtil
     {
+        /// <summary>
+        /// 判断入参 <c>s</c> 是否为 <c>null</c>
+        /// </summary>
+        /// <param name="s">待判断的字符串</param>
+        /// <returns>若 <c>s</c> 为 <c>null</c> 返回 <c>true</c>，否则返回 <c>false</c></returns>
+        public static bool IsNull(string s)
+        {
+            return s == null;
+        }
     }
 }
