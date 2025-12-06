9fee2c020ae3237073e20e9af915721c8addc07b
bitm <1759743261@qq.com>
2025-12-06 13:26:56 +0800

    新增：随机生成 11 位手机号码（MockUtil.GetRandomPhoneNumber）

--- ETool.Core/Util/MockUtil.cs
+++ ETool.Core/Util/MockUtil.cs
@@ -5,5 +5,21 @@
     /// </summary>
     public static class MockUtil
     {
+        /// <summary>
+        /// 生成随机的 11 位手机号码
+        /// </summary>
+        /// <returns>随机生成的 11 位手机号码</returns>
+        public static string GetRandomPhoneNumber()
+        {
+            char[] resultChars = new char[11];
+            resultChars[0] = '1';
+            resultChars[1] = RandomUtil.GetRandomDigitChar(3, 9);
+            for (int i = 2; i < 11; i++)
+            {
+                resultChars[i] = RandomUtil.GetRandomDigitChar(0, 9);
+            }
+
+            return new string(resultChars);
+        }
     }
 }
