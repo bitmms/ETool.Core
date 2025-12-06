ee6133cfdb7d0b34f72145d17dd58622c5dd6b1d
bitm <1759743261@qq.com>
2025-12-06 11:22:23 +0800

    新增：线程安全的 Random 实例（RandomThreadLocal）

--- ETool.Core/Util/RandomUtil.cs
+++ ETool.Core/Util/RandomUtil.cs
@@ -1,9 +1,16 @@
-﻿namespace ETool.Core.Util
+﻿using System;
+using System.Threading;
+
+namespace ETool.Core.Util
 {
     /// <summary>
     /// 随机生成工具类
     /// </summary>
     public static class RandomUtil
     {
+        /// <summary>
+        /// 每一个线程本地存储的 Random 实例【保证多线程环境下随机数生成的线程安全与随机性】
+        /// </summary>
+        private static readonly ThreadLocal<Random> RandomThreadLocal = new ThreadLocal<Random>(() => new Random(Guid.NewGuid().GetHashCode()));
     }
 }
