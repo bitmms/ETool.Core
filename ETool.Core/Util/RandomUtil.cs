7f51d6b54ec6560b9391c24d0f24058c94fca862
bitm <1759743261@qq.com>
2025-12-09 23:07:39 +0800

    新增: 获取一个随机的数字字符【RandomUtil.GetRandomDigitChar(int minValue = 0, int maxValue = 9)】

--- ETool.Core/Util/RandomUtil.cs
+++ ETool.Core/Util/RandomUtil.cs
@@ -43,5 +43,31 @@ namespace ETool.Core.Util
         {
             return GetRandomInt(0, 1) == 0;
         }
+
+        /// <summary>
+        /// 获取一个随机的数字字符
+        /// </summary>
+        /// <param name="minValue">最小的随机字符对应的数值（包含），默认值为 0</param>
+        /// <param name="maxValue">最大的随机字符对应的数值（包含），默认值为 9</param>
+        /// <returns>介于 <c>minValue</c> 和 <c>maxValue</c> 之间的随机数字字符</returns>
+        public static char GetRandomDigitChar(int minValue = 0, int maxValue = 9)
+        {
+            // 负数自动修正为 0
+            minValue = Math.Max(minValue, 0);
+            maxValue = Math.Max(maxValue, 0);
+
+            // 自动修正参数顺序
+            NumberUtil.SwapIfFirstLarger(ref minValue, ref maxValue);
+
+            char[] defaultChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            // 超出有效范围时：返回 0-9 的随机字符
+            if (minValue > 9)
+            {
+                return defaultChars[GetRandomInt(0, 9)];
+            }
+
+            return defaultChars[GetRandomInt(minValue, maxValue)];
+        }
     }
 }
