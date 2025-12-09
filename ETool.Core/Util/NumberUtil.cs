f25e79f1d1b4d49e7dc1d5fff288ae3371451881
bitm <1759743261@qq.com>
2025-12-09 22:32:36 +0800

    新增: 比较两个整数的大小并按需交换二者的数值【NumberUtil.SwapIfFirstLarger(ref int x1, ref int x2)】

--- ETool.Core/Util/NumberUtil.cs
+++ ETool.Core/Util/NumberUtil.cs
@@ -17,5 +17,18 @@
                 CommonUtil.Swap(ref x1, ref x2);
             }
         }
+
+        /// <summary>
+        /// 比较 <c>x1</c> 和 <c>x2</c>，当 <c>x1 &gt; x2</c> 时交换二者的值
+        /// </summary>
+        /// <param name="x1">第一个整数（ref 类型，最终存储较小值）</param>
+        /// <param name="x2">第二个整数（ref 类型，最终存储较大值）</param>
+        public static void SwapIfFirstLarger(ref int x1, ref int x2)
+        {
+            if (x1 > x2)
+            {
+                CommonUtil.Swap(ref x1, ref x2);
+            }
+        }
     }
 }
