74d7aa2f71c4504872944d36af9b87371d26e699
bitm <1759743261@qq.com>
2025-12-09 22:05:43 +0800

    新增: 判断指定字符串是否不为空【StringUtil.IsNotEmpty(string s)】

--- ETool.Core/Util/StringUtil.cs
+++ ETool.Core/Util/StringUtil.cs
@@ -34,5 +34,15 @@
         {
             return s == string.Empty;
         }
+
+        /// <summary>
+        /// 判断指定字符串是否不为空
+        /// </summary>
+        /// <param name="s">待判断的字符串</param>
+        /// <returns>如果字符串不为空返回 true，否则返回 false</returns>
+        public static bool IsNotEmpty(string s)
+        {
+            return s != string.Empty;
+        }
     }
 }
