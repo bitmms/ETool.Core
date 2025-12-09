cea04de7badf203bb84c17e156724eec0ac46c42
bitm <1759743261@qq.com>
2025-12-09 22:16:44 +0800

    新增: 判断指定字符是否为数字【CharUtil.IsDigit(char c)】

--- ETool.Core/Util/CharUtil.cs
+++ ETool.Core/Util/CharUtil.cs
@@ -5,5 +5,14 @@
     /// </summary>
     public static class CharUtil
     {
+        /// <summary>
+        /// 判断指定字符是否为数字
+        /// </summary>
+        /// <param name="c">待判断的字符</param>
+        /// <returns>如果字符为数字返回 true，否则返回 false</returns>
+        public static bool IsDigit(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
     }
 }
