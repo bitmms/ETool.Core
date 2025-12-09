26b62301282d51fa618dd656efb504272c95fa1c
bitm <1759743261@qq.com>
2025-12-09 22:06:25 +0800

    新增: 判断指定字符串是否为 null 或为空【StringUtil.IsNullOrEmpty(string s)】

--- ETool.Core/Util/StringUtil.cs
+++ ETool.Core/Util/StringUtil.cs
@@ -44,5 +44,15 @@
         {
             return s != string.Empty;
         }
+
+        /// <summary>
+        /// 判断指定字符串是否为 null 或为空
+        /// </summary>
+        /// <param name="s">待判断的字符串</param>
+        /// <returns>如果字符串为 null 或为空返回 true，否则返回 false</returns>
+        public static bool IsNullOrEmpty(string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
     }
 }
