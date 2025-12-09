6d8567526b3d9e59d6bed1c2661f6bbb3a348351
bitm <1759743261@qq.com>
2025-12-09 22:04:42 +0800

    新增: 判断指定字符串是否不为 null【StringUtil.IsNotNull(string s)】

--- ETool.Core/Util/StringUtil.cs
+++ ETool.Core/Util/StringUtil.cs
@@ -14,5 +14,15 @@
         {
             return s == null;
         }
+
+        /// <summary>
+        /// 判断指定字符串是否不为 null
+        /// </summary>
+        /// <param name="s">待判断的字符串</param>
+        /// <returns>如果字符串不为 null 返回 true，否则返回 false</returns>
+        public static bool IsNotNull(string s)
+        {
+            return s != null;
+        }
     }
 }
