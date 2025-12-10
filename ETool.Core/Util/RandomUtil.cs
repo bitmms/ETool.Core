2faa82ef0b74ba951d30e9d5457282c8b354d83f
bitm <1759743261@qq.com>
2025-12-10 22:33:01 +0800

    新增: 获取一个指定格式的随机 GUID 字符串【RandomUtil.GetGuidString(string format = "D")】

--- ETool.Core/Util/RandomUtil.cs
+++ ETool.Core/Util/RandomUtil.cs
@@ -105,5 +105,46 @@ namespace ETool.Core.Util
 
             return new string(resultChars);
         }
+
+        /// <summary>
+        /// 获取一个指定格式的随机 GUID 字符串
+        /// </summary>
+        /// <param name="format">规则字符串</param>
+        /// <returns>符合格式规则的 GUID 字符串（异常场景返回全 0 的 GUID 字符串）</returns>
+        public static string GetGuidString(string format = "D")
+        {
+            // null：返回全 0 GUID 字符串
+            if (format == null)
+            {
+                return "00000000-0000-0000-0000-000000000000";
+            }
+
+            // 空字符串：返回 D 格式 UUID 字符串
+            if (format.Length == 0)
+            {
+                return Guid.NewGuid().ToString("D");
+            }
+
+            // 小写 -> 大写
+            format = StringUtil.ToUpperLetter(format);
+
+            // 合法 GUID 格式符集合
+            const string roleString = "DNBPX";
+
+            // 双字符规则：相同且为 DNBPX 中任意一个 → 返回大写 GUID 字符串
+            if (format.Length == 2 && format[0] == format[1] && roleString.Contains(format[0].ToString()))
+            {
+                return StringUtil.ToUpperLetter(Guid.NewGuid().ToString(format[0].ToString()));
+            }
+
+            // 单字符规则：为 DNBPX 中任意一个  → 返回小写 GUID 字符串
+            if (format.Length == 1 && roleString.Contains(format))
+            {
+                return Guid.NewGuid().ToString(format);
+            }
+
+            // 其他情况：返回全 0 GUID 字符串
+            return "00000000-0000-0000-0000-000000000000";
+        }
     }
 }
