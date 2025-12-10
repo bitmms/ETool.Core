06fc5d5dad29118816dc466f52586adad5197ce2
bitm <1759743261@qq.com>
2025-12-10 22:12:21 +0800

    新增: 手机号码脱敏处理【MaskUtil.MaskPhoneNumber(string phoneNumber, char maskChar = '*')】

--- ETool.Core/Util/MaskUtil.cs
+++ ETool.Core/Util/MaskUtil.cs
@@ -5,5 +5,25 @@
     /// </summary>
     public static class MaskUtil
     {
+        /// <summary>
+        /// 手机号码脱敏处理：保留前3位和后4位，中间4位替换为指定掩码字符
+        /// </summary>
+        /// <param name="phoneNumber">待脱敏的手机号码字符串</param>
+        /// <param name="maskChar">用于替换的填充字符</param>
+        /// <returns>脱敏后的字符串</returns>
+        public static string MaskPhoneNumber(string phoneNumber, char maskChar = '*')
+        {
+            if (StringUtil.IsNull(phoneNumber))
+            {
+                return "";
+            }
+
+            if (ValidatorUtil.IsValidPhoneNumber(phoneNumber))
+            {
+                return StringUtil.ReplaceRangeWithChar(phoneNumber, 3, 4, maskChar);
+            }
+
+            return phoneNumber;
+        }
     }
 }
