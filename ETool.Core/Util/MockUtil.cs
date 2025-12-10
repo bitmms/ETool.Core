e3337da9e728b4ffb9ce5ced4cceff0ad0cfa327
bitm <1759743261@qq.com>
2025-12-10 22:21:30 +0800

    新增: 生成模拟的 11 位手机号码【MockUtil.MockPhoneNumber()】

--- ETool.Core/Util/MockUtil.cs
+++ ETool.Core/Util/MockUtil.cs
@@ -5,5 +5,26 @@
     /// </summary>
     public static class MockUtil
     {
+        #region 生成模拟的 11 位手机号码
+
+        /// <summary>
+        /// 生成模拟的 11 位手机号码
+        /// </summary>
+        /// <returns>模拟生成的 11 位手机号码</returns>
+        public static string MockPhoneNumber()
+        {
+            char[] resultChars = new char[11];
+
+            resultChars[0] = '1';
+            resultChars[1] = RandomUtil.GetRandomDigitChar(3, 9);
+            for (int i = 2; i < 11; i++)
+            {
+                resultChars[i] = RandomUtil.GetRandomDigitChar(0, 9);
+            }
+
+            return new string(resultChars);
+        }
+
+        #endregion
     }
 }
