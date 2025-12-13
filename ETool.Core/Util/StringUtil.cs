3285802091d2f1e4a629745e83e3555093e79ad7
bitm <1759743261@qq.com>
2025-12-13 23:49:34 +0800

    新增: 将使用 Unicode 字符集表示的字符串以指定编码格式保存到字节数组【StringUtil.GetUtf_8_16_32Bytes(string s)】

--- ETool.Core/Util/StringUtil.cs
+++ ETool.Core/Util/StringUtil.cs
@@ -101,6 +101,42 @@ namespace ETool.Core.Util
         }
 
         /// <summary>
+        /// 将使用 Unicode 字符集表示的字符串以 UTF8 编码保存到字节数组
+        /// </summary>
+        /// <remarks>无字节序问题，1~4字节的变长编码格式</remarks>
+        /// <param name="s">入参字符串</param>
+        /// <returns>字节数组</returns>
+        public static byte[] GetUtf8Bytes(string s)
+        {
+            if (IsNullOrEmpty(s)) return Array.Empty<byte>();
+            return Encoding.UTF8.GetBytes(s);
+        }
+
+        /// <summary>
+        /// 将使用 Unicode 字符集表示的字符串以 UTF16 编码保存到字节数组
+        /// </summary>
+        /// <remarks>小端序在前，2字节（BMP字符）或4字节（补充平面字符）的变长编码格式</remarks>
+        /// <param name="s">入参字符串</param>
+        /// <returns>字节数组</returns>
+        public static byte[] GetUtf16Bytes(string s)
+        {
+            if (IsNullOrEmpty(s)) return Array.Empty<byte>();
+            return Encoding.Unicode.GetBytes(s);
+        }
+
+        /// <summary>
+        /// 将使用 Unicode 字符集表示的字符串以 UTF32 编码保存到字节数组
+        /// </summary>
+        /// <remarks>小端序在前，固定4字节的编码格式</remarks>
+        /// <param name="s">入参字符串</param>
+        /// <returns>字节数组</returns>
+        public static byte[] GetUtf32Bytes(string s)
+        {
+            if (IsNullOrEmpty(s)) return Array.Empty<byte>();
+            return Encoding.UTF32.GetBytes(s);
+        }
+
+        /// <summary>
         /// 将字符串中的小写英文字符转大写
         /// </summary>
         /// <param name="s">待转换的字符串</param>
