3e1a3620c34ae2fe355743c1703bb0ac92d784a7
bitm <1759743261@qq.com>
2025-12-15 17:25:00 +0800

    创建: 身份证号码工具类【IdCardUtil】

---
+++ ETool.Core/Util/IdCardUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace ETool.Core.Util
+{
+    /// <summary>
+    /// 身份证号码工具类【中国大陆身份证号码：18位、15位】
+    /// </summary>
+    /// <remarks>参考标准：<see href="https://openstd.samr.gov.cn/bzgk/gb/newGbInfo?hcno=080D6FBF2BB468F9007657F26D60013E">《GB11643-1999》</see></remarks>
+    public static class IdCardUtil
+    {
+    }
+}
