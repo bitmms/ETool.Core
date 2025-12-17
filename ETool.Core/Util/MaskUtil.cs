fc14736f8f7b55d81fee2f5072c81d0b9acad240
bitm <1759743261@qq.com>
2025-12-17 11:47:30 +0800

    新增: 特殊的身份证号码脱敏处理【MaskUtil.MaskIdCard(string idCard, char maskChar = '*')】

--- ETool.Core/Util/MaskUtil.cs
+++ ETool.Core/Util/MaskUtil.cs
@@ -25,5 +25,31 @@
 
             return phoneNumber;
         }
+
+        /// <summary>
+        /// 身份证号码脱敏处理：保留前3位和后3位，中间部分替换为指定掩码字符【兼容15、18位身份证号码】
+        /// </summary>
+        /// <param name="idCard">待脱敏的身份证号码字符串</param>
+        /// <param name="maskChar">用于替换的填充字符</param>
+        /// <returns>脱敏后的字符串</returns>
+        public static string MaskIdCard(string idCard, char maskChar = '*')
+        {
+            if (StringUtil.IsNull(idCard))
+            {
+                return "";
+            }
+
+            if (!IdCardUtil.IsValidChinaIdCard(idCard))
+            {
+                return idCard;
+            }
+
+            if (idCard.Length == 18)
+            {
+                return StringUtil.ReplaceRangeWithChar(idCard, 3, 12, maskChar);
+            }
+
+            return StringUtil.ReplaceRangeWithChar(idCard, 3, 9, maskChar);
+        }
     }
 }
