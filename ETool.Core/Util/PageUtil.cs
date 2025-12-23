4d6b5f22fe4f87b1bfec70ae9fdfc73eb5f3567a
bitm <1759743261@qq.com>
2025-12-23 09:39:59 +0800

    新增: 计算分页的开始索引（包含）【PageUtil.GetStartIndex(int pageNumber, int pageSize)】

--- ETool.Core/Util/PageUtil.cs
+++ ETool.Core/Util/PageUtil.cs
@@ -44,5 +44,48 @@ namespace ETool.Core.Util
 
             return totalCount == 0 ? 0 : (totalCount - 1) / pageSize + 1;
         }
+
+        /// <summary>
+        /// 计算分页的开始索引（包含）
+        /// </summary>
+        /// <param name="pageNumber">当前页码</param>
+        /// <param name="pageSize">每页包含的元素数量</param>
+        /// <returns>分页的开始索引（包含）</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><c>pageNumber</c> 小于等于 0</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><c>pageSize</c> 小于等于 0</exception>
+        /// <exception cref="OverflowException">计算得出的开始索引超出了 int 的范围</exception>
+        /// <example>
+        /// <code>
+        /// GetStartIndex(1, 10) → 0
+        /// GetStartIndex(2, 10) → 10
+        /// GetStartIndex(3, 5)  → 10
+        /// </code>
+        /// </example>
+        public static int GetStartIndex(int pageNumber, int pageSize)
+        {
+            if (pageNumber <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(pageNumber),
+                    pageNumber,
+                    $"当前页码 '{nameof(pageNumber)}' 必须大于 0，实际值：{pageNumber}");
+            }
+
+            if (pageSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(pageSize),
+                    pageSize,
+                    $"每页大小 '{nameof(pageSize)}' 必须大于 0，实际值：{pageSize}");
+            }
+
+            var startIndexLong = (long)(pageNumber - 1) * pageSize;
+            if (startIndexLong > int.MaxValue)
+            {
+                throw new OverflowException($"分页开始索引溢出超出 int 范围：(pageNumber - 1) * pageSize = ({pageNumber} - 1) * {pageSize} = {startIndexLong} > int.MaxValue = {int.MaxValue}");
+            }
+
+            return (int)startIndexLong;
+        }
     }
 }
