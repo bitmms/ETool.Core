ab33ba9592cee5e4525d87093cfbc7dc97dd2191
bitm <1759743261@qq.com>
2025-12-26 09:06:31 +0800

    新增: 从指定格式的日期字符串中解析该日期是当前月中的第几天【DateTimeUtil.GetDayOfMonth(string s, params string[] formats)】

--- ETool.Core/Util/DateTimeUtil.cs
+++ ETool.Core/Util/DateTimeUtil.cs
@@ -80,6 +80,20 @@ namespace ETool.Core.Util
         }
 
         /// <summary>
+        /// 从指定格式的日期字符串中解析该日期是当前月中的第几天
+        /// </summary>
+        /// <param name="s">要解析的日期字符串</param>
+        /// <param name="formats">用于解析的一个或多个日期格式字符串</param>
+        /// <returns>返回该日期在当前月中的第几天（范围：1 到 31）</returns>
+        /// <exception cref="ArgumentException"><c>s == null || s == ""</c></exception>
+        /// <exception cref="ArgumentException"><c>formats == null || formats.Length == 0</c></exception>
+        /// <exception cref="FormatException">字符串 <c>s</c> 无法按 <c>formats</c> 提供的任一格式解析为有效日期</exception>
+        public static int GetDayOfMonth(string s, params string[] formats)
+        {
+            return ToDateTime(s, formats).Day;
+        }
+
+        /// <summary>
         /// 判断 DateTime 是否在指定的起止日期时间内（包含边界）
         /// </summary>
         /// <param name="dt">要判断的日期时间</param>
